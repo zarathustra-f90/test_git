77e0b7c3072599443f6aa586d85d27ba65d12731
Linux Nomad <linuxnomad@fedorabudgie.home>
2024-06-30 21:13:50 +0300

    hello world request

--- hello_world.php
+++ hello_world.php
@@ -1,3 +1,12 @@
 <?php
-echo "Hello, World!";
+echo "Enter your name: ";
+$username = trim(fgets(STDIN));
+
+echo "Hello, $username!\n";
+
+echo "Enter your age:\n";
+$userage = trim(fgets(STDIN));
+
+echo "Hello, $username!\n";
+echo "You are $userage years old!\n";
 ?>
